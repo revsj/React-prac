30c409f90b47aa06bd932f18203f72d5ae447b05
revsj <revojalandoni5@gmail.com>
2025-09-16 22:49:48 +0800

    prog2

--- react-tuts/src/components/ListGroup.tsx
+++ react-tuts/src/components/ListGroup.tsx
@@ -2,16 +2,22 @@
 
 
 function ListGroup() {
-  const items = ["New York", "Philippines", "Singapore", "Paranaque", "Paris"];
+  let items = ["New York", "Philippines", "Singapore", "Paranaque", "Paris"];
+
 
 
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 ? <p>No item found</p> : null} 
+      {items.length === 0 && <p>No item found</p> }
       <ul className="list-group">
         {items.map((item) => (
-          <li>{item}</li>
+          <li className="list-group-item" key={item} onClick={() => console.log(item)}
+      >    
+      {item}
+      </li>
         ))}
       </ul>
     </>
